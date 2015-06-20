9065b0509b91d5e414975a0507d0af8f4003cfbb
Cristiano Betta <cbetta@gmail.com>
2015-06-20 07:47:16 +0200

    Specifying starting point for demo

--- app.rb
+++ app.rb
@@ -1,25 +1,13 @@
 require 'sinatra'
 require 'braintree'
 
-Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = 'ffdqc9fyffn7yn2j'
-Braintree::Configuration.public_key = 'qj65nndbnn6qyjkp'
-Braintree::Configuration.private_key = 'a3de3bb7dddf68ed3c33f4eb6d9579ca'
+# TODO: Add API Keys
 
 get '/' do
-  @client_token = Braintree::ClientToken.generate
+  # TODO: Create client token
   erb :index
 end
 
 post '/pay' do
-  result = Braintree::Transaction.sale(
-    amount: 10,
-    payment_method_nonce: params[:payment_method_nonce],
-  )
-
-  if result.success?
-    "Transaction ID: #{result.transaction.id}"
-  else
-    "We did something wrong"
-  end
+  # TODO: Create sale
 end
